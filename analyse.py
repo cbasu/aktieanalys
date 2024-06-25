fdeacf3fda31a6ef094833a384e719050a0d26dd
cbasu <cbasu.pers@gmail.com>
2024-06-25 12:07:40 +0200

    v14

--- analyse.py
+++ analyse.py
@@ -10,9 +10,13 @@ from tabulate import tabulate
 begin = "2021-01-01"
 today = datetime.today().date()
 
-tickers = ['8TRA', 'ABB', 'ATRLJ-B', 'BOL', 'BONAV-B', 'CARE', 'DOM', 'EKTA-B', 'EPI-A', 'ERIC-B', 'INDU-C', 'INVE-B', 'JM', 'NIBE-B', 'PEAB-B', 'REJL-B', 'SHB-B', 'SSAB-B', 'SWED-A', 'TEL2-B', 'TELIA', 'VIVA', 'VOLCAR-B', 'VOLV-B']
-#tickers = ['8TRA']
+exchange = {
+        "ST": ['8TRA', 'AAK', 'ABB', 'ATCO-B', 'ATRLJ-B', 'BOL', 'BONAV-B', 'CARE', 'COIC', 'DOM', 'EKTA-B', 'EPI-A', 'ERIC-B', 'ESSITY-B', 'HTRO', 'INDU-C', 'INVE-B', 'JM', 'NIBE-B', 'PEAB-B', 'REJL-B', 'SAND', 'SHB-B', 'SKF-B', 'SSAB-B', 'SWED-A', 'TEL2-B', 'TELIA', 'VIVA', 'VOLCAR-B', 'VOLV-B'],
+        #"ST": ['8TRA'],
+        "OL": []
+    }
 
+#tickers = ['8TRA']
 
 #sorted_list = sorted(tickers)
 #print(sorted_list)
@@ -28,37 +32,45 @@ def completer(text, state):
 
 user_input = input("Run analysis (y/n): ")
 if user_input == "y":
-    for stock in tickers:
-        start = begin
-        end = today
-        file = "yfdata/"+stock+".json"
-        d = utils.rd_d(file)
-        if d:
-            start = d["Date"][len(d["Date"])-1]
-            date_object = datetime.strptime(start, "%Y-%m-%d")
-            date_object = date_object + timedelta(days=1)
-            start = date_object.strftime("%Y-%m-%d")
+    #for stock in tickers:
+    for key in exchange:
+        tickers = exchange[key]
+        for stock in tickers:
+            start = begin
+            end = today
+            nam = stock+"."+key
+            file = "yfdata/"+nam+".json"
+            d = utils.rd_d(file)
+            if d:
+                start = d["Date"][len(d["Date"])-1]
+                date_object = datetime.strptime(start, "%Y-%m-%d")
+                date_object = date_object + timedelta(days=1)
+                start = date_object.strftime("%Y-%m-%d")
 
-        # Fetch historical stock data
-        df = yf.download(stock+".ST", start=start, end=end)
-        d = utils.append_yf2d(df, d)
+            # Fetch historical stock data
+            df = yf.download(nam, start=start, end=end)
+            d = utils.append_yf2d(df, d)
         
-        utils.analyse(stock, 60, d)
-        utils.analyse(stock, 120, d)
-        utils.analyse(stock, 360, d)
+            utils.analyse(stock, 60, d)
+            utils.analyse(stock, 120, d)
+            utils.analyse(stock, 360, d)
 
-        # Write the dictionary to a JSON file
-        with open(file, 'w') as json_file:
-            json.dump(d, json_file, indent=4)
+            # Write the dictionary to a JSON file
+            with open(file, 'w') as json_file:
+                json.dump(d, json_file, indent=4)
 
 table = []
-for stock in tickers:
-    fname = "yfdata/" + stock + ".json"
-    d = utils.rd_d(fname)
-    reco = utils.is_close_to_max_min(d["Slope60"])
-    if reco == "neutral":
-        continue
-    table.append([stock, reco])
+#for stock in tickers:
+for key in exchange:
+    tickers = exchange[key]
+    for stock in tickers:
+        nam = stock+"."+key
+        fname = "yfdata/" + nam + ".json"
+        d = utils.rd_d(fname)
+        reco = utils.is_close_to_max_min(d["Slope60"])
+        if reco == "neutral":
+            continue
+        table.append([nam, reco])
 
 # Sort data by the 2nd column (index 1)
 table = sorted(table, key=lambda x: x[1])
@@ -67,8 +79,12 @@ headers = ["Name", "Rec60"]
 print(tabulate(table, headers=headers, tablefmt="plain"))
 
 commands=["exit"]
-for name in tickers:
-    commands.append(name)
+#for name in tickers:
+for key in exchange:
+    tickers = exchange[key]
+    for stock in tickers:
+        nam = stock+"."+key
+        commands.append(nam)
 
 # Configure readline to use the completer function
 readline.set_completer(completer)
@@ -80,7 +96,10 @@ while True:
     user_input = input("Enter: ")
     if user_input == 'exit':
         break
-    if user_input in tickers:
+    inp = user_input.split('.')[0]
+    key = user_input.split('.')[1]
+    tickers = exchange[key]
+    if inp in tickers:
         fname = "yfdata/" + user_input + ".json"
         d = utils.rd_d(fname)
         utils.plot(user_input, d)
